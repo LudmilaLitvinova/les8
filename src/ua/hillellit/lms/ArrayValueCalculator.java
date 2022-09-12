54e742a91eb6c675373e25462601a7acf3eb2f1a
LudmilaLitvinova <ludik-1984@ukr.net>
2022-09-12 22:42:35 +0300

    Refactor try-catch

--- src/ua/hillellit/lms/ArrayValueCalculator.java
+++ src/ua/hillellit/lms/ArrayValueCalculator.java
@@ -6,13 +6,12 @@ import ua.hillellit.lms.exceptions.ArraySizeException;
 public class ArrayValueCalculator {
 
   public static void main(String[] args) {
-    String[][] strAr = {{"1","2","3","4"},{"1","5","6","7"},{"2","7","8","9"},{"3","edc","8","7"}};
+    String[][] strAr = {{"1", "2", "3", "4"}, {"1", "5", "6", "7"}, {"2", "7", "8", "9"},
+        {"3", "edc", "8", "7"}};
 
     try {
       System.out.println(doCalc(strAr));
-    } catch (ArraySizeException e) {
-      System.out.println(e.getMessage());
-    } catch (ArrayDataException e){
+    } catch (ArraySizeException | ArrayDataException e) {
       System.out.println(e.getMessage());
     }
 
@@ -27,16 +26,16 @@ public class ArrayValueCalculator {
         throw new ArraySizeException("Масив повинен бути 4 на 4 ");
       }
     }
-    int sumAllElements=0;
+    int sumAllElements = 0;
     int x;
-    for (int i=0;i<ar.length;i++){
-      for (int j=0;j<ar[i].length;j++){
-        try{
-        x =Integer.parseInt(ar[i][j]);}
-        catch (NumberFormatException e){
-          throw new ArrayDataException(i+1,j+1);
+    for (int i = 0; i < ar.length; i++) {
+      for (int j = 0; j < ar[i].length; j++) {
+        try {
+          x = Integer.parseInt(ar[i][j]);
+        } catch (NumberFormatException e) {
+          throw new ArrayDataException(i + 1, j + 1);
         }
-        sumAllElements+=x;
+        sumAllElements += x;
       }
 
     }

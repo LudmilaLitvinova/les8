31c0a60232cc436654264c72f98241b50ad3b5b5
LudmilaLitvinova <ludik-1984@ukr.net>
2022-09-17 14:41:01 +0300

    Fix null check

--- src/ua/hillellit/lms/ArrayValueCalculator.java
+++ src/ua/hillellit/lms/ArrayValueCalculator.java
@@ -6,23 +6,29 @@ import ua.hillellit.lms.exceptions.ArraySizeException;
 public class ArrayValueCalculator {
 
   public static void main(String[] args) {
-    String[][] strAr = {{"1", "2", "3", "4"}, {"2", "7", "8", "9"}, {"3", "5", "8", "9"},
-        {"3", "5", "8", "апр"}};
+    String[][] strAr = {{"1", "2", "3", "4"}, {"2", "7", "8", "9"}, {"3", "7", "8", "9"},
+        {"4", "5", "8", "fgh"}};
 
     try {
       System.out.println(doCalc(strAr));
     } catch (ArraySizeException | ArrayDataException e) {
       System.out.println(e.getMessage());
     }
-
   }
 
   static int doCalc(String[][] ar) throws ArraySizeException, ArrayDataException {
+    if (ar == null) {
+      throw new ArraySizeException("Масив повинен бути 4 на 4. Ви передали null-масив");
+    }
     if (ar.length != 4) {
       throw new ArraySizeException("Масив повинен бути 4 на 4. Невірна кількість стрічок. "
           + "У даному масиві їх кількість дорівнює " + ar.length);
     }
     for (int i = 0; i < ar.length; i++) {
+      if (ar[i] == null) {
+        throw new ArraySizeException(
+            "Масив не повинен мати null. Ви передали null у " + (i + 1) + " стрічці");
+      }
       if (ar[i].length != 4) {
         throw new ArraySizeException("Масив повинен бути 4 на 4. Невірна кількість комірок у "
             + (i + 1) + " стрічці");
@@ -41,10 +47,8 @@ public class ArrayValueCalculator {
         }
         sumAllElements += x;
       }
-
     }
     return sumAllElements;
   }
-
 }
 
